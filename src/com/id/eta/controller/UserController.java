2fdd5e6de302697a1a2e3f9d438376b64ab3a59f
drunklite <drunklite@gmail.com>
2015-09-05 20:58:30 +0800

    no message

--- src/com/id/eta/controller/UserController.java
+++ src/com/id/eta/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.id.eta.service.UserService;
@@ -25,6 +26,7 @@ public class UserController {
 		return mv;
 	}
 
+	// @ResponseBody
 	@RequestMapping(value = "/count", method = RequestMethod.GET)
 	public ModelAndView count() {
 
